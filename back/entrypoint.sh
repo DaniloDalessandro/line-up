a0ee46a781f961140d602b200a7d11aae0b4f3d0
danilodalessandro <danilodalessandro08@gmail.com>
2026-03-14 02:19:15 -0300

    Use runserver to debug startup (temp)

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -4,11 +4,15 @@ echo "==> Waiting for database..."
 sleep 5
 
 echo "==> Running migrations..."
-python manage.py migrate --noinput
-if [ $? -ne 0 ]; then
-    echo "ERROR: migrations failed!"
+python manage.py migrate --noinput 2>&1
+MIGRATE_EXIT=$?
+echo "==> Migration exit code: $MIGRATE_EXIT"
+
+if [ $MIGRATE_EXIT -ne 0 ]; then
+    echo "ERROR: migrations failed! Sleeping to allow log inspection..."
+    sleep 3600
     exit 1
 fi
 
-echo "==> Starting gunicorn..."
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 1 --timeout 120 --log-level debug
+echo "==> Starting server..."
+exec python manage.py runserver 0.0.0.0:8000
