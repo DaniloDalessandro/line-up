8da9d167f6ac1719903494a2818df28e0044d3f3
danilodalessandro <danilodalessandro08@gmail.com>
2026-03-14 02:15:20 -0300

    Reduce gunicorn to 1 worker, increase timeout, add debug logging

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-set -e
 
 echo "==> Waiting for database..."
-sleep 3
+sleep 5
 
 echo "==> Running migrations..."
 python manage.py migrate --noinput
+if [ $? -ne 0 ]; then
+    echo "ERROR: migrations failed!"
+    exit 1
+fi
 
 echo "==> Starting gunicorn..."
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 1 --timeout 120 --log-level debug
